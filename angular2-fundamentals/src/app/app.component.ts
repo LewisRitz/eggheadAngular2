1c0d5f63044ea4cb09ef4f44d1c27f2052f31f2e
Lewis Ritz <lritz229@gmail.com>
2016-12-27 11:24:55 -0500

    first 4 videos

--- angular2-fundamentals/src/app/app.component.ts
+++ angular2-fundamentals/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:
+    `<div>
+        <app-simple-form></app-simple-form>
+    </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = `Let's get started!`;
 }
