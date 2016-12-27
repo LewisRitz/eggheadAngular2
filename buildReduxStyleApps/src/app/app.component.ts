e882a7abcf3884da07c9f536f759672456ec54a5
Lewis Ritz <lritz229@gmail.com>
2016-12-27 15:29:23 -0500

    starting to work with rxjs

--- buildReduxStyleApps/src/app/app.component.ts
+++ buildReduxStyleApps/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {Observable, Subject} from "rxjs";
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <button (click)="click$.next()">Update</button>
+    <h1>{{clock | async | date:'EEEE, MMMM d, y, h:m:s a'}}</h1>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  click$ = new Subject();
+
+  clock;
+
+  constructor(){
+    this.clock = this.click$.map(() => new Date());
+  }
 }
